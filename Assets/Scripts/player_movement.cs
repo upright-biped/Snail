7526f3a8c7091377e675550cfbb1028525b82ddd
alivzqz <37880317+alivzqz@users.noreply.github.com>
2019-11-18 17:49:32 -0600

    Extra Assets
    Assets Used for level creation

---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+
+    public float Speed = 10.0f;
+    public float Gravity = 9.0f;
+
+    private CharacterController C_Controller;
+    private Vector3 moveDir = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        C_Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (C_Controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= Speed;
+        }
+
+        moveDir.y -= Gravity * Time.deltaTime;
+        C_Controller.Move(moveDir * Time.deltaTime);
+    }
+}
