e50d76c8839e858c33804911a997e4995630d357
dhuynh502 <43050362+dhuynh502@users.noreply.github.com>
2019-11-19 17:09:07 -0600

    Created Character info
    Armor
    Health
    Damage
    Name

---
+++ Assets/Scripts/character_info.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_info : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        snail_info MainChar = gameObject.AddComponent<snail_info>();
+        MainChar.Name = "Player 1";
+        MainChar.Health = 100;
+        MainChar.Armor = 100;
+        MainChar.Damage = 10;
+        snail_info SnailChar = gameObject.AddComponent < snail_info>();
+        SnailChar.Name = "Snail 1";
+        SnailChar.Health = 10;
+        SnailChar.Armor = 10;
+        SnailChar.Damage = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/snail_info.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snail_info : MonoBehaviour
+{
+    public string Name { get; set; }
+    public float Health { get; set; }
+    public float Armor { get; set; }
+    public float Damage { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
