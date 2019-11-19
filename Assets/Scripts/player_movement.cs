6d3ffacb5383d5e257d7dfd86d9380b065d8ab96
alivzqz <37880317+alivzqz@users.noreply.github.com>
2019-11-18 20:07:00 -0600

    Updated Water Movement Script
    Created Illusion of moving underwater. Added ability for player to run w/ Adrenaline.

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
 
-    public float Speed = 10.0f;
-    public float Gravity = 9.0f;
+    public float Speed = 7.0f;
+    public float Gravity = 7.0f;
+    public float RotationSpeed = 7.0f;
+    public float JumpSpeed = 8.0f;
+    public float RunSpeed = 14.0f;
 
     private CharacterController C_Controller;
     private Vector3 moveDir = Vector3.zero;
+    private float yaw = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +24,34 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (C_Controller.isGrounded)
+        if (C_Controller.isGrounded && Input.GetKey(KeyCode.LeftShift))
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= RunSpeed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDir.y = JumpSpeed;
+            }
+        }
+        
+        if (C_Controller.isGrounded && !Input.GetKey(KeyCode.LeftShift))
         {
             moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDir = transform.TransformDirection(moveDir);
             moveDir *= Speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDir.y = JumpSpeed;
+            }
         }
 
         moveDir.y -= Gravity * Time.deltaTime;
         C_Controller.Move(moveDir * Time.deltaTime);
+
+        yaw = RotationSpeed * Input.GetAxis("Mouse X");
+        transform.Rotate(new Vector3(0.0f, yaw, 0.0f), Space.World);
     }
 }
