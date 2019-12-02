7792eec8c3b1cb3a518ce2333bc6b5c2b6d29b4f
upright-biped <52828757+upright-biped@users.noreply.github.com>
2019-12-02 13:14:42 -0600

    spawning fixed
    despawns when moused over not just clicked

---
+++ Assets/Creatures/Crawling/Crawling.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crawling : MonoBehaviour
+{
+    public int speed;
+    GameObject player;
+    Spawning spawnScript;
+    Inventory inventory;
+    float dist;
+    Ray ray;
+    RaycastHit hit;
+
+    void Start()
+    {
+        player = GameObject.Find("SpawnCenter");
+        spawnScript = player.GetComponent<Spawning>();
+        inventory = player.GetComponent<Inventory>();
+    }
+
+    void Update()
+    {
+        //Always facing camera
+        gameObject.transform.LookAt(GameObject.Find("SpawnCenter").transform.position);
+        //Despawn when camera out of range or when clicked, decrease count in spawning script
+        dist = Vector3.Distance(player.transform.position, this.transform.position);
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//clicking??
+        if (dist<4 && Physics.Raycast(ray, out hit) && hit.collider.tag == "Creatures")
+        {
+//add to inventory
+
+            spawnScript.count--;
+            Destroy(gameObject);
+            Destroy(this);
+        }
+
+        if(dist>20)
+        {
+            spawnScript.count--;
+            Destroy(gameObject);
+            Destroy(this);
+        }
+//moving to random points tilting/speed
+
+
+
+//sprite flipping
+
+
+
+        //maintaining distance from camera
+        //distance from landmarks or other creatures
+
+    }
+}
+
---
+++ Assets/Creatures/Spawning.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public int count;
+    public GameObject[] species;
+    public int lastSea;
+    Vector3 playerLocation;
+    int randCreature;
+    bool stop = false;
+
+    public float height;
+    public float xPoint;
+    public float yPoint;
+    public float zPoint;
+    void Start()
+    {
+        StartCoroutine(WaitSpawner());
+    }
+
+    void Update()
+    {
+        //location of player
+        playerLocation = GameObject.Find("SpawnCenter").transform.position;
+    }
+
+    IEnumerator WaitSpawner()
+    {
+        while (!stop)
+        {
+            //choose creature to spawn
+            if (GameObject.Find("Sea Floor")==true)
+            {
+                randCreature = Random.Range(0, lastSea);
+            }
+            else
+            {
+                randCreature = Random.Range(lastSea, species.Length);
+            }
+
+            //get radius of that species
+            height = species[randCreature].GetComponent<SphereCollider>().radius;
+            //choose random cooridnates
+            xPoint = Random.Range(playerLocation.x - 20, playerLocation.x + 20);
+            zPoint = Random.Range(playerLocation.z - 20, playerLocation.z + 20);
+            yPoint = height+1;
+            Vector3 spawnPosition = new Vector3(xPoint, yPoint, zPoint);
+
+            Instantiate(species[randCreature], spawnPosition, gameObject.transform.rotation);
+
+            count++;
+            yield return new WaitForSeconds(5);
+            //pauses script if 12 or more creatures are instantiated
+            while(count>12)
+            {
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+    }
+
+}
