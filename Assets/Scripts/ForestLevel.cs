a99a531dbedb02212c75f8a51f4666e5ccfbe7e0
dhuynh502 <43050362+dhuynh502@users.noreply.github.com>
2019-12-01 19:33:00 -0600

    Menu changing scenes

---
+++ Assets/Scripts/ForestLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForestLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ForestPlay()
+    {
+        // SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+        SceneManager.LoadScene("Forest");
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    // Use this for initialization
+    void Start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+    public void PlayGame()
+    {
+       // SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+        SceneManager.LoadScene("Underwater");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
