0d7d8523efca1fd790f23ff1bd0b6ba4732f5ade
dhuynh502 <43050362+dhuynh502@users.noreply.github.com>
2019-12-02 18:25:21 -0600

    GameOver Scence

---
+++ Assets/BackTo1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BackTo1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void WaterPlay()
+    {
+        // SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+        SceneManager.LoadScene("Underwater");
+    }
+    public void QuitPlay()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
