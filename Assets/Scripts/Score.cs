947011d91278dd177c5d4954839fbcbe2088db73
dhuynh502 <43050362+dhuynh502@users.noreply.github.com>
2019-12-02 18:02:20 -0600

    ScoreBoard

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Text thisText;
+    private int score;
+
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+
+        // set score value to be zero
+        score = 0;
+    }
+
+    void Update()
+    {
+        // When P is hit
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            // add 500 points to score
+            score += 500;
+        }
+        // update text of Text element
+        thisText.text = "SCORE: " + score;
+    }
+}
