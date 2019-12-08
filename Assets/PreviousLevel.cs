4ec35c1387f1373122598fda00c0167f6fae9445
dhuynh502 <43050362+dhuynh502@users.noreply.github.com>
2019-12-07 19:40:55 -0600

    More UI

--- Assets/PreviousLevel.cs
+++ Assets/PreviousLevel.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PreviousLevel : MonoBehaviour
 {
+    private Canvas CanvasObject;
+
+    public void Start()
+    {
+        CanvasObject = GameObject.Find("TUTORIALS").GetComponent<Canvas>();
+        CanvasObject.GetComponent<Canvas>().enabled = false;
+    }
     // Start is called before the first frame update
     public void WaterPlay()
     {
@@ -14,5 +21,18 @@ public class PreviousLevel : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+    public void Tutorials()
+    {
+        if (CanvasObject.enabled == true)
+        {
+            CanvasObject.GetComponent<Canvas>().enabled = false;
+            //MainMenuCanvas.GetComponent<Canvas>().enabled = true;
+        }
+        else
+        {
+            CanvasObject.GetComponent<Canvas>().enabled = true;
+            //MainMenuCanvas.GetComponent<Canvas>().enabled = false;
+        }
     }
 }
